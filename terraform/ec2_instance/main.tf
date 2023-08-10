8188dd183e9d9ecfcdd06fd4ec5f97216de90a26
dolii <darya.olejnik@gmail.com>
2023-08-10 16:15:23 +0300

    Moved credentials variableto a different file

--- terraform/ec2_instance/main.tf
+++ terraform/ec2_instance/main.tf
@@ -1,57 +1,25 @@
 provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = "us-east-1"
 }
-
-resource "aws_vpc" "vpc" {
-    cidr_block       = "10.0.0.0/16"
-    instance_tenancy = "default"
-
-    tags = "${var.default_tags}"
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-resource "aws_subnet" "subnet" {
-    vpc_id     = aws_vpc.vpc.id
-    cidr_block = "10.0.0.0/24"
-    availability_zone = "us-east-1e"
-    map_public_ip_on_launch = true
-
-    tags = "${var.default_tags}"
-    depends_on = [aws_internet_gateway.igw]
-}
-
 resource "tls_private_key" "rsa-4096" {
-    algorithm = "RSA"
-    rsa_bits  = 4096
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "ssh_key" {
-    key_name   = "${var.ami_key_pair_name}"
-    public_key = tls_private_key.rsa-4096.public_key_openssh
-    tags       = "${var.default_tags}"
+  key_name   = var.ami_key_pair_name
+  public_key = tls_private_key.rsa-4096.public_key_openssh
+  tags       = var.default_tags
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami             = "${var.ami_id}"
-    count           = "${var.number_of_instances}"
-    subnet_id       = "${aws_subnet.subnet.id}"
-    instance_type   = "${var.instance_type}"
-    private_ip      = "${var.instance_private_ip}"
-    key_name        = "${var.ami_key_pair_name}"
-    user_data       = "${file("../ec2-user-data.sh")}"
-    tags            = "${var.default_tags}"
+  ami           = var.ami_id
+  count         = var.number_of_instances
+  subnet_id     = var.subnet_id
+  instance_type = var.instance_type
+  key_name      = var.ami_key_pair_name
+  user_data     = file("../ec2-user-data.sh")
+  tags          = var.default_tags
 }   
-
-resource "aws_eip" "eip" {
-  domain = "vpc"
-
-  instance                  = aws_instance.ec2_instance[0].id
-  associate_with_private_ip = "${var.instance_private_ip}"
-  tags                      = "${var.default_tags}"
-  depends_on                = [aws_internet_gateway.igw]
-}
---
+++ terraform/ec2_instance/terraform.tfvars
@@ -0,0 +1,9 @@
+#====== Instance Variables =========
+instance_name       = "awsbuilder-demo"
+instance_type       = "t2.micro"
+ami_id              = "ami-0f34c5ae932e6f0e4"
+number_of_instances = 1
+ami_key_pair_name   = "httpd"
+
+#====== Network Variables ==========
+subnet_id = "subnet-02a4260b7f2470c65"
